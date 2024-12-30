854e63c25448d55b467ee0e83642b91b93f99eb0
Anuj Kumar Mishra <144154519+iamakmishra11@users.noreply.github.com>
2024-12-30 22:42:24 +0530

    Added Codes

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,16 @@ import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
 function App() {
+    // Correctly select the todos from the todoState slice of the Redux store
+    const todos = useSelector(state => state.todoState.todos);
 
-  const todos = useSelector(state => state.todoState.todos)
-
-  return (
-    <>
-    <h1>Todo App</h1>
-    <TodoInput/>
-    <TodoList todos={todos} />
-
-    </>
-  )
-    
-  
+    return (
+        <>
+            <h1>Todo App</h1>
+            <TodoInput />
+            <TodoList todos={todos} />
+        </>
+    );
 }
 
-export default App
+export default App;
